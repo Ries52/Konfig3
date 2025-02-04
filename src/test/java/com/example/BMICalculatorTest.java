78e46d4b0c821170ab6da5681f2fe599616f1e98
micha <52415753@edu.campus02.at>
2025-02-04 20:38:11 +0100

    changed

--- src/test/java/com/example/BMICalculatorTest.java
+++ src/test/java/com/example/BMICalculatorTest.java
@@ -1,38 +1,36 @@
 package com.example;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 
 public class BMICalculatorTest {
     @Test
     public void testConstructor() {
         BMICalculator calculator = new BMICalculator("Max", "Mustermann", 170, 70, 'm');
-        assertEquals("Max", calculator.getFirstname());
-        assertEquals("Mustermann", calculator.getLastname());
-        assertEquals(170, calculator.getBodyHeight());
-        assertEquals(70, calculator.getBodyWeight(), 0.01);
-        assertEquals('m', calculator.getGender());
+        Assertions.assertEquals("Max", calculator.getFirstname());
+        Assertions.assertEquals("Mustermann", calculator.getLastname());
+        Assertions.assertEquals(170, calculator.getBodyHeight());
+        Assertions.assertEquals(70, calculator.getBodyWeight(), 0.01);
+        Assertions.assertEquals('m', calculator.getGender());
     }
 
     @Test
     public void testCalculateBMI() {
         BMICalculator calculator = new BMICalculator("Max", "Mustermann", 170, 70, 'm');
         double bmi = calculator.calculateBMI();
-        assertEquals(24.22, bmi, 0.01);
+        Assertions.assertEquals(24.22, bmi, 0.01);
     }
     @Test
     public void testCalculateBMICategory() {
         BMICalculator calculator = new BMICalculator("Max", "Mustermann", 170, 70, 'm');
         int category = calculator.calculateBMICategory();
-        assertEquals(1, category); // Normalgewicht
+        Assertions.assertEquals(0, category); // Normalgewicht
     }
     @Test
     public void testGetBMICategoryName() {
         BMICalculator calculator = new BMICalculator("Max", "Mustermann", 170, 70, 'm');
         String categoryName = calculator.getBMICategoryName();
-        assertEquals("Normalgewicht", categoryName);
+        Assertions.assertEquals("Normalgewicht", categoryName);
     }
 
 }
